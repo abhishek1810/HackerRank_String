f8eb5b152ec0fb5d6efb542b128a9e85dbdc65c3
Naveen Jakkani <iamnani@users.noreply.github.com>
2016-04-06 21:41:37 +0530

    Dynamic Programming basic example
    Dynamic Programming basic example

---
+++ algorithms/src/DP/DP_Fibonacci.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package DP;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * @author JakkaniN
+ *
+ */
+public class DP_Fibonacci {
+
+	/**
+	 * 
+	 */
+	public DP_Fibonacci() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		String[] input = s.nextLine().split(" ");
+		
+		BigInteger a = new BigInteger(input[0]);
+		BigInteger b = new BigInteger(input[1]);
+		int n = Integer.parseInt(input[2]);
+		
+		BigInteger nth = BigInteger.ZERO;
+		
+		for (int i = 3; i <= n; i++) {
+			nth = b.multiply(b).add(a);
+			a = b;
+			b = nth;
+		}
+		
+		System.out.println(nth);
+		
+		
+		s.close();
+		
+	}
+
+}
